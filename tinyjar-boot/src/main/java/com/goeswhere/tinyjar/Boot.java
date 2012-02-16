d1c959a613e7bb4966387ad0d19f44769de7fbc5
Chris West (Faux) <git@goeswhere.com>
2012-02-16 18:48:52 +0000

    No manifest, corrupt pack file allegedly

--- tinyjar-boot/src/main/java/com/goeswhere/tinyjar/Boot.java
+++ tinyjar-boot/src/main/java/com/goeswhere/tinyjar/Boot.java
@@ -1,7 +1,22 @@
 package com.goeswhere.tinyjar;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Pack200;
+
+import lzma.sdk.lzma.Decoder;
+import lzma.streams.LzmaInputStream;
+
 public class Boot {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		final File tmp = File.createTempFile("data", "jar");
+		Pack200.newUnpacker().unpack(new LzmaInputStream(Boot.class.getResourceAsStream("data"), new Decoder()), new JarOutputStream(new FileOutputStream(tmp)));
+		final URLClassLoader cl = new URLClassLoader(new URL[] { tmp.toURI().toURL() });
+		final Class<?> cls = cl.loadClass(String.valueOf(new JarFile(tmp).getManifest().getMainAttributes().get("Main-Class")));
+        cls.getMethod("main", new Class[]{String[].class}).invoke(null, new Object[]{args});
 	}
 }
--- tinyjar-package/src/main/java/com/goeswhere/tinyjar/Packager.java
+++ tinyjar-package/src/main/java/com/goeswhere/tinyjar/Packager.java
@@ -1,5 +1,97 @@
 package com.goeswhere.tinyjar;
 
-public class Packager {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import java.util.jar.Pack200;
+import java.util.jar.Pack200.Packer;
+import java.util.zip.ZipEntry;
 
+import lzma.streams.LzmaOutputStream;
+
+public class Packager {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		if (args.length != 1 || (args.length != 0 && (args[0].equals("--help") || args[0].equals("-h")))) {
+			System.err.println("usage: jarfile");
+			System.exit(1);
+			return;
+		}
+
+		final File inFile = new File(args[0]);
+
+		final Manifest mf = new Manifest();
+		final JarOutputStream jos = new JarOutputStream(new FileOutputStream(inFile.getAbsolutePath() + ".jar"), mf);
+		try {
+			final InputStream boot = Packager.class.getResourceAsStream("/tinyjar-boot.jar");
+			final JarInputStream jis = new JarInputStream(boot);
+			ZipEntry en;
+			while (null != (en = jis.getNextEntry())) {
+				jos.putNextEntry(en);
+				copy(jis, jos);
+			}
+
+			jos.putNextEntry(new ZipEntry("data"));
+
+			final FileInputStream fis = new FileInputStream(inFile);
+			try {
+				final JarInputStream jarIn = new JarInputStream(fis);
+				try {
+					try {
+						final LzmaOutputStream lzma = new LzmaOutputStream.Builder(jos).build();
+						try {
+							final Packer packer = Pack200.newPacker();
+							packer.addPropertyChangeListener(new PropertyChangeListener() {
+								public void propertyChange(PropertyChangeEvent evt) {
+									if (!Packer.PROGRESS.equals(evt.getPropertyName()))
+										return;
+									final String val = String.valueOf(evt.getNewValue());
+									System.err.printf("jar200: %3s%% complete..\n", val);
+
+									if ("100".equals(val))
+										System.err.println("Awaiting lzma completion...");
+								}
+							});
+							packer.pack(jarIn, lzma);
+						} finally {
+							lzma.flush();
+							lzma.close();
+						}
+					} finally {
+					}
+				} finally {
+					jarIn.close();
+				}
+			} finally {
+				fis.close();
+			}
+		} finally {
+			jos.flush();
+			jos.close();
+		}
+
+		System.out.println("Done.");
+	}
+
+	public static long copy(InputStream from, OutputStream to) throws IOException {
+		byte[] buf = new byte[4096];
+		long total = 0;
+		while (true) {
+			int r = from.read(buf);
+			if (r == -1) {
+				break;
+			}
+			to.write(buf, 0, r);
+			total += r;
+		}
+		return total;
+	}
 }
