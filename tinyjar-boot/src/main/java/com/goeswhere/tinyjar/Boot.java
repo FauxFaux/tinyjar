0dcb62629def451b3cf3de68ce7aa351e33af234
Chris West (Faux) <git@goeswhere.com>
2012-02-23 23:06:44 +0000

    Boot closes resources, and sets thread classloader (fixes Spring)

--- tinyjar-boot/src/main/java/com/goeswhere/tinyjar/Boot.java
+++ tinyjar-boot/src/main/java/com/goeswhere/tinyjar/Boot.java
@@ -2,6 +2,7 @@ package com.goeswhere.tinyjar;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.jar.JarFile;
@@ -13,10 +14,55 @@ import lzma.streams.LzmaInputStream;
 
 public class Boot {
 	public static void main(String[] args) throws Exception {
-		final File tmp = File.createTempFile("data", "jar");
-		Pack200.newUnpacker().unpack(new LzmaInputStream(Boot.class.getResourceAsStream("data"), new Decoder()), new JarOutputStream(new FileOutputStream(tmp)));
-		final URLClassLoader cl = new URLClassLoader(new URL[] { tmp.toURI().toURL() });
-		final Class<?> cls = cl.loadClass(String.valueOf(new JarFile(tmp).getManifest().getMainAttributes().get("Main-Class")));
-        cls.getMethod("main", new Class[]{String[].class}).invoke(null, new Object[]{args});
+		final File tmp = File.createTempFile("data", ".jar");
+		System.out.println("Unpacking to: " + tmp.getAbsolutePath() + "...");
+
+		final FileOutputStream fos = new FileOutputStream(tmp);
+		try {
+			final JarOutputStream jos = new JarOutputStream(fos);
+			try {
+				final InputStream data = Boot.class.getResourceAsStream("/data");
+				try {
+					final LzmaInputStream in = new LzmaInputStream(data, new Decoder());
+					try {
+						Pack200.newUnpacker().unpack(in, jos);
+					} finally {
+						in.close();
+					}
+				} finally {
+					data.close();
+				}
+			} finally {
+				jos.flush();
+				jos.close();
+			}
+		} finally {
+			fos.flush();
+			fos.close();
+		}
+
+		System.out.println("  done, loading...");
+
+		final String mainClass;
+		final JarFile jf = new JarFile(tmp);
+		try {
+			mainClass = String.valueOf(jf.getManifest().getMainAttributes().getValue("Main-Class"));
+		} finally {
+			jf.close();
+		}
+
+		callMain(args, tmp, mainClass);
+	}
+
+	static void callMain(String[] args, final File url, final String mainClass) throws Exception {
+		final URLClassLoader cl = new URLClassLoader(new URL[] { url.toURI().toURL() }, Boot.class.getClassLoader()) {
+			@Override
+			protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+				Thread.currentThread().setContextClassLoader(this);
+				return super.loadClass(name, resolve);
+			}
+		};
+
+		cl.loadClass(mainClass).getMethod("main", new Class[] { String[].class }).invoke(null, new Object[] { args });
 	}
 }
